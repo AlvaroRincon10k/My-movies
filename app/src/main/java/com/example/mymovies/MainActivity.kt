c03715c25819b404229a5452dad0c5b340aee5df
Alvaro Rincon <alvarorinconc1@gmail.com>
2025-03-29 19:46:07 -0500

    Peticiones en un hilo secundario.
    - Se actualiza la vista principal con los datos obtenidos de la API "themoviedb".
    - Se completa la URL de las imágenes, concatenándole una URL base.
    - Se elimina el código innecesario.

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mymovies
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mymovies.databinding.ActivityMainBinding
@@ -14,25 +13,21 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recycler.adapter = MoviesAdapter(
-            listOf(
-                Movie("Title 1", "https://loremflickr.com/320/240?lock=1"),
-                Movie("Title 2", "https://loremflickr.com/320/240?lock=2"),
-                Movie("Title 3", "https://loremflickr.com/320/240?lock=3"),
-                Movie("Title 4", "https://loremflickr.com/320/240?lock=4"),
-                Movie("Title 5", "https://loremflickr.com/320/240?lock=5"),
-                Movie("Title 6", "https://loremflickr.com/320/240?lock=6")
-            )
-        ) { movie ->
+        val moviesAdapter = MoviesAdapter(emptyList()) { movie ->
             Toast.makeText(this@MainActivity, movie.title, Toast.LENGTH_SHORT).show()
         }
+        binding.recycler.adapter = moviesAdapter
 
         thread {
             val apiKey = getString(R.string.api_key)
             val popularMovies = MovieDbClient.service.listPopularMovies(apiKey)
             val body = popularMovies.execute().body()
-            if (body != null)
-                Log.d("Alvaro", "Movie count: ${body.results.size}")
+
+            runOnUiThread {
+                if (body != null)
+                    moviesAdapter.movies = body.results
+                moviesAdapter.notifyDataSetChanged()
+            }
         }
     }
 }
--- app/src/main/java/com/example/mymovies/MoviesAdapter.kt
+++ app/src/main/java/com/example/mymovies/MoviesAdapter.kt
@@ -5,9 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.mymovies.databinding.ViewMovieItemBinding
+import com.example.mymovies.model.Movie
 
 class MoviesAdapter(
-    private val movies: List<Movie>,
+    var movies: List<Movie>,
     private val movieClickedListener: (Movie) -> Unit
 ) :
     RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
@@ -31,7 +32,8 @@ class MoviesAdapter(
 
         fun bing(movie: Movie) {
             binding.title.text = movie.title
-            Glide.with(binding.root.context).load(movie.cover).into(binding.cover)
+            Glide.with(binding.root.context)
+                .load("https://image.tmdb.org/t/p/w185/${movie.poster_path}").into(binding.cover)
         }
     }
 }
