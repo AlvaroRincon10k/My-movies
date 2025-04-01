f78a393aa0172e88d74f91097e429906d6c62fd3
Alvaro Rincon <alvarorinconc1@gmail.com>
2025-03-31 19:42:33 -0500

    Creación de la Detail Activity.
    - Se crea la activity "DetailActivity" para el detalle de cada película.
    - Se configura el binding en la activity "DetailActivity"
    - Se agrega un ImageView y un TextView en la activity "DetailActivity" para el detalle de cada película.

---
+++ app/src/main/java/com/example/mymovies/DetailActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mymovies
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mymovies.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
