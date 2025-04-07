a19629588aa20b258b5632d4f2c8620d3fcb13b6
Alvaro Rincon <alvarorinconc1@gmail.com>
2025-04-06 20:18:44 -0500

    Vista personalizada para imágenes.
    - Se crea el archivo "attrs" con el atributo ratio para el tamaño de la imagen.
    - Se crea la clase "AspectRationImageView" la cual se encarga de personalizar el tamaño de la vista para la imagen.
    - Se utiliza "AspectRationImageView" como imageView para cada película, asignándole un valor de 1.5 a la propiedad ratio.

---
+++ app/src/main/java/com/example/mymovies/AspectRationImageView.kt
@@ -0,0 +1,37 @@
+package com.example.mymovies
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class AspectRationImageView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs) {
+
+    var ratio: Float = 1f
+
+    init {
+        val a = context.obtainStyledAttributes(attrs, R.styleable.AspectRationImageView)
+        ratio = a.getFloat(R.styleable.AspectRationImageView_ratio, 1f)
+        a.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        var width = measuredWidth
+        var height = measuredHeight
+
+        if (width == 0 && height == 0) {
+            return
+        }
+
+        if (width > 0) {
+            height = (width * ratio).toInt()
+        } else if (height > 0) {
+            width = (height / ratio).toInt()
+        }
+
+        setMeasuredDimension(width, height)
+    }
+}
